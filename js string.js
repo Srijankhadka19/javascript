157f04b6ac978a4b2e2917cb9b980dfc776e0f89
Srijan khadka <99087250+Srijankhadka19@users.noreply.github.com>
2022-11-24 19:06:02 +0545

    js string

---
+++ js string.js
@@ -0,0 +1,29 @@
+var myFirstString = "I Love JavaScript"
+
+console.log(typeof(myFirstString))
+var mySecondString = "we will learln javascript together"
+
+console.log(typeof(mySecondString))
+
+//what if my string contains a double quotes
+
+var doubleQuotesString = "this is \"javaScript\" string"
+
+console.log(doubleQuotesString)
+
+//what if my string contains single Quotes
+
+var singleQuotesString = 'This is \'jacaScript\' string'
+
+console.log(singleQuotesString)
+
+//another method to write double quotes in string
+
+var singleQuotesString1 = "This is 'javaScript' string"
+console.log(singleQuotesString1)
+
+//another method to write  single quotes in string
+
+var singleQuotesString2 = 'This is "javaScript" string'
+console.log(singleQuotesString2)
+
